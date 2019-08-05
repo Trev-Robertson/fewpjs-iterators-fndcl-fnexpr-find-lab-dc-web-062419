ca6f04b7569ec2ad800a271e2db5d87fa5b68da1
Trevahr Robertson <Trevahr@gmail.com>
2019-08-05 17:32:17 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+
+
+function superbowlWin(arr){
+  if (arr.find(function(e) {return e.result === "W"}))  {
+      return arr.find(function(e) {return e.result === "W"}).year}
+  else {
+      return undefined
+    }
+}
